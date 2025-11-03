3d836710349f31c12f8df5e231367d5c10cf0a1d
Lakshay Batra <lakshaynandu.batraben10@gmail.com>
2025-11-03 21:50:27 +0530

    Multiple Stage Builds

---
+++ 4-multi-stage-builds/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+// Set up mongoose connection
+async function connect(){
+  console.log(process.env.MONGO_URI)
+  await mongoose.connect(process.env.MONGO_URI, {});
+}
+
+connect();
+
+const EntrySchema = new mongoose.Schema({
+  text: String,
+  date: { type: Date, default: Date.now },
+});
+
+const Entry = mongoose.model('Entry', EntrySchema);
+
+app.get('/', async (req, res) => {
+  try {
+    const entry = new Entry({ text: 'This is an entry by harkirat' });
+    await entry.save();
+    res.send('Entry added!');
+  } catch (err) {
+    res.status(500).send('Error occurred');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
